caa3945f62ccd87f2f1ffb64cb50de58cd3c34f6
danialazarnia <danialazarnia83@gmail.com>
2023-05-27 12:46:50 +0330

    some change in map

--- WeakholdCampaigner/src/main/java/view/GameMenu.java
+++ WeakholdCampaigner/src/main/java/view/GameMenu.java
@@ -1,12 +1,19 @@
 package view;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -21,6 +28,8 @@ import static model.game.map.MapCell.Texture.*;
 
 public class GameMenu extends Application {
     private GridPane gridPane;
+    private ScrollPane scrollPane;
+    private StackPane gamePane;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -31,10 +40,15 @@ public class GameMenu extends Application {
         Database.loadMap();
         createMap(Database.getMapById(1));
 
-        ScrollPane scrollPane = new ScrollPane(gridPane);
+        scrollPane = new ScrollPane(gridPane);
         scrollPane.setPrefViewportWidth(40 * 20); // 40 cells wide (20 pixels each)
         scrollPane.setPrefViewportHeight(40 * 20); // 40 cells high (20 pixels each)
-        Scene scene = new Scene(scrollPane);
+        setZoom(scrollPane);
+
+        gamePane = new StackPane(scrollPane);
+        Scene scene = new Scene(gamePane,stage.getMaxWidth(),stage.getMaxHeight());
+        scrollPane.setPrefSize(stage.getMaxWidth(),stage.getMaxHeight());
+
         stage.setScene(scene);
         stage.setFullScreen(true);
         stage.show();
@@ -50,7 +64,7 @@ public class GameMenu extends Application {
         for (int i = 0; i < 200; i++) {
             for (int j = 0; j < 200; j++) {
                 Rectangle cell = new Rectangle(60, 60);
-                cell.setFill(imagePatternHashMap.get(map.getCell(i,j).getTexture()));
+                cell.setFill(imagePatternHashMap.get(map.getCell(i, j).getTexture()));
                 Group group = new Group(cell);
                 gridPane.add(group, i, j);
             }
@@ -59,49 +73,50 @@ public class GameMenu extends Application {
         cell.setFill(new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/1.png")
                 .toExternalForm())));
-        addInMap(cell,150,3);
+        addInMap(cell, 150, 3);
     }
+
     private void setImagePattern(HashMap<MapCell.Texture, ImagePattern> imagePatternHashMap) {
-        imagePatternHashMap.put(BEACH,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(BEACH, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/BEACH.jpg")
                 .toExternalForm())));
-        imagePatternHashMap.put(DEEP_WATER,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(DEEP_WATER, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/DEEP_WATER.jpg")
                 .toExternalForm())));
-        imagePatternHashMap.put(GRASS,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(GRASS, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/GRASS.jpg")
                 .toExternalForm())));
-        imagePatternHashMap.put(GRASSLAND,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(GRASSLAND, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/GRASSLAND.jpg")
                 .toExternalForm())));
-        imagePatternHashMap.put(GRAVEL,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(GRAVEL, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/GRAVEL.jpg")
                 .toExternalForm())));
-        imagePatternHashMap.put(IRON,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(IRON, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/IRON.jpg")
                 .toExternalForm())));
-        imagePatternHashMap.put(LAND,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(LAND, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/LAND.jpg")
                 .toExternalForm())));
-        imagePatternHashMap.put(MEADOW,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(MEADOW, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/MEADOW.jpg")
                 .toExternalForm())));
-        imagePatternHashMap.put(OIL,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(OIL, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/OIL.jpg")
                 .toExternalForm())));
-        imagePatternHashMap.put(PLAIN,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(PLAIN, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/PLAIN.jpg")
                 .toExternalForm())));
-        imagePatternHashMap.put(RIVER,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(RIVER, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/RIVER.jpg")
                 .toExternalForm())));
-        imagePatternHashMap.put(SHALLOW_WATER,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(SHALLOW_WATER, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/SHALLOW_WATER.jpg")
                 .toExternalForm())));
-        imagePatternHashMap.put(SLATE,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(SLATE, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/SLATE.jpg")
                 .toExternalForm())));
-        imagePatternHashMap.put(STONE,new ImagePattern(new Image(GameMenu.class
+        imagePatternHashMap.put(STONE, new ImagePattern(new Image(GameMenu.class
                 .getResource("/Tiles/STONE.jpg")
                 .toExternalForm())));
     }
@@ -116,5 +131,38 @@ public class GameMenu extends Application {
         Group group = (Group) gridPane.getChildren().get(gridPane.getRowCount() * j + i);
         group.getChildren().remove(node);
     }
+
+    public void setZoom(ScrollPane scrollPane) {
+        double minScale = 0.8;
+        double maxScale = 1.5;
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setOnScroll(new EventHandler<ScrollEvent>() {
+            @Override
+            public void handle(ScrollEvent event) {
+                double zoomFactor = 1.05;
+                double deltaY = event.getDeltaY();
+
+                if (deltaY < 0) {
+                    zoomFactor = 1 / 1.05;
+                }
+
+                // Check if zoom out will cause the scale to become negative
+                double newScaleX = scrollPane.getScaleX() * zoomFactor;
+                double newScaleY = scrollPane.getScaleY() * zoomFactor;
+                if (newScaleX < minScale || newScaleY > maxScale) {
+                    return;
+                }
+
+
+                scrollPane.setScaleX(newScaleX);
+                scrollPane.setScaleY(newScaleY);
+
+                event.consume();
+            }
+        });
+
+    }
+
 }
 
